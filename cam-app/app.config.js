bafd06f85db339a814c314a316ba9da2895bc8c3
Borna <niknahad.borna@gmail.com>
2025-05-28 20:21:36 -0700

    setup app for local installation

--- cam-app/app.config.js
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-
-export default ({ config }) => ({
-  ...config,
-  extra: {
-    SUPABASE_URL: process.env.SUPABASE_URL,
-    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
-  },
-});
